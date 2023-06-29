deeec02c3f0738a5d58cd80154ee010c7f64091e
Noah <nlhutchin@icloud.com>
2023-06-29 15:00:02 -0600

    complete tab 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
-
+const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ",puppies);
   return (
-    <>
-      <div>
-        
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
-    </>
-  )
+  );
 }
 
 export default App
